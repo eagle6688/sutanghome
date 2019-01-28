acb12c38595c2997e37815e8e68eb32d805a86f6
Aldwin Su <fangyansu@163.com>
2019-01-28 21:58:59 +0800

    Finish ValidationAspect

--- src/main/java/com/sutanghome/common/aop/ValidationAspect.java
+++ src/main/java/com/sutanghome/common/aop/ValidationAspect.java
@@ -6,12 +6,31 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import devutility.external.javax.validation.ValidationUtils;
+import devutility.external.javax.validation.annotation.Validation;
+import devutility.external.javax.validation.model.ValidationResult;
+import devutility.internal.models.OperationResult;
+
 @Aspect
 @Order(3)
 @Component
 public class ValidationAspect {
 	@Around(value = "com.sutanghome.common.aop.Pointcuts.pointCutForAction()")
 	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-		return null;
+		Object[] args = joinPoint.getArgs();
+
+		for (Object param : args) {
+			if (param != null && param.getClass().isAnnotationPresent(Validation.class)) {
+				ValidationResult validationResult = ValidationUtils.validate(param);
+
+				if (validationResult.isFailed()) {
+					OperationResult result = new OperationResult();
+					result.setErrorMessage(validationResult.getFirstErrorMessage());
+					return result;
+				}
+			}
+		}
+
+		return joinPoint.proceed();
 	}
 }
