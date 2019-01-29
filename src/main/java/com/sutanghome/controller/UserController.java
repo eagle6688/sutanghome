fb3cd1b44b8e41e5f7d19ce9b00aa30eb70985b1
Aldwin Su <fangyansu@163.com>
2019-01-29 20:35:15 +0800

    Update

--- src/main/java/com/sutanghome/controller/UserController.java
+++ src/main/java/com/sutanghome/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sutanghome.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController extends BaseController {
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("title", "User list!");
 		return "/user/index";
 	}
 }
