dc3da7c772b8977595535918c50ce6efaaf62e8f
Aldwin Su <fangyansu@163.com>
2019-02-02 00:04:06 +0800

    Update

--- src/main/view/templates/user/index/script/index.js
+++ src/main/view/templates/user/index/script/index.js
@@ -11,6 +11,7 @@ var page = {
 page.init = function () {
     selector.init();
     this.init_formModal();
+    this.bind();
 };
 
 page.init_formModal = function () {
@@ -33,4 +34,33 @@ page.init_formModal = function () {
     });
 };
 
-//page.init();
+page.bind = function () {
+    page.bind_list();
+    page.bind_addBtn();
+};
+
+page.bind_list = function () {
+    $('#pagination').pagination({
+        buttonClass: 'page-item',
+        buttonAClass: 'page-link',
+        paginationClass: 'pagination justify-content-center',
+        onPageClick: function (pageIndex) {
+            vueHelper.changePage(pageIndex);
+        }
+    });
+
+    vueHelper = $('#div-list').vueHelper({
+        url: this.url_list,
+        onReload: function (data) {
+            $('#pagination').data('pagination').changeTotalRecords(data.count);
+        }
+    });
+};
+
+page.bind_addBtn = function () {
+    $('#btn-add').click(function () {
+        SaveForm.prev_add(formModal);
+    });
+};
+
+page.init();
