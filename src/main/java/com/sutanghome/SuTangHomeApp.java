65ee3ac81b1fc4ca3c58f661d7aa8ec392e47747
Aldwin Su <fangyansu@163.com>
2019-02-12 14:33:38 +0800

    Update

--- src/main/java/com/sutanghome/SuTangHomeApp.java
+++ src/main/java/com/sutanghome/SuTangHomeApp.java
@@ -2,20 +2,13 @@ package com.sutanghome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:system.properties")
-public class SuTangHomeApp extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SuTangHomeApp.class);
-	}
-
+public class SuTangHomeApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SuTangHomeApp.class, args);
-		System.out.println("Starting World!");
+		System.out.println("Starting......");
 	}
 }
